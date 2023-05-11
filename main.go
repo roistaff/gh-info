a55d5150d8febdc45ea79977d87f9cde1616e098
roistaff <roistaff1983@gmail.com>
2023-05-11 18:27:37 +0900

    okay.

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
         "encoding/json"
         "fmt"
-        "io/ioutil"
         "log"
-        "net/http"
         "os"
-        "github.com/fatih/color"
         "strconv"
+        "github.com/fatih/color"
+        "net/http"
+	"io/ioutil"
+        "strings"
 )
 type UserResponse struct {
         Company    string `json:"company"`
@@ -30,12 +31,11 @@ type OwnerInfo struct {
 	Name string `json:"login"`
 	}
 type RepoResponse struct {
-	Fullname   string `json:"fullname"`
+	Fullname   string `json:"full_name"`
 	URL        string `json:"html_url"`
 	Fork       bool   `json:"fork"`
 	Update     string `json:"pushed_at"`
 	Lang       string `json:"language"`
-	Homepage   string `json:"homepage"`
 	Message    string `json:"message"`
 	License    LicenseInfo `json:"license"`
 	Owner      OwnerInfo `json:"owner"`
@@ -44,14 +44,16 @@ type RepoResponse struct {
 	Description string `json:"description"`
 	Star       int    `json:"stargazers_count"`
 }
+
 const url string = "https://api.github.com/"
 
 func getAPI(url2 string) []byte {
-        resp, _ := http.Get(url + url2)
-        defer resp.Body.Close()
-        body, _ := ioutil.ReadAll(resp.Body)
-        return body
+	resp, _ := http.Get(url + url2)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return body
 }
+
 func getInfoRepo(fullname string){
 	var data RepoResponse
 	body := getAPI("repos/" +fullname)
@@ -62,7 +64,8 @@ func getInfoRepo(fullname string){
 		color.Red("Repo '"+fullname+"' Not Found")
 		os.Exit(1)
 	}
-	fmt.Println(data.Owner)
+	datalist := color.GreenString("\n"+data.Fullname)+"\t\t"+color.YellowString("☆Star:")+strconv.Itoa(data.Star)+color.HiMagentaString("\nDescription: ")+data.Description+color.HiCyanString("\nLanguage: ")+data.Lang+color.BlueString("\nLicense: ")+data.License.Name+color.MagentaString("\nURL: ")+data.URL
+	fmt.Println(datalist)
 }
 func getInfoUser(username string){
         var data UserResponse
@@ -79,14 +82,39 @@ func getInfoUser(username string){
         fmt.Println(datalist)
 	}
 }
-
+func welcome(){
+        welcometext := `         ____  _  _    _             _       ___          __        
+        / ___|(_)| |_ | |__   _   _ | |__   |_ _| _ __   / _|  ___  
+       | |  _ | || __|| '_ \ | | | || '_ \   | | | '_ \ | |_  / _ \ 
+       | |_| || || |_ | | | || |_| || |_) |  | | | | | ||  _|| (_) |
+        \____||_| \__||_| |_| \__,_||_.__/  |___||_| |_||_|   \___/ 
+                                                                    `
+        fmt.Println(welcometext)
+}
+func Helps(message ...string){
+        if message != nil{
+                fmt.Println(color.RedString("! Message: "),message)
+        }
+        fmt.Println("Hello")
+}
 func main() {
         color.HiGreen("Github Info")
         args := os.Args
-	if len(args) != 1{
+	if len(args) != 1{if len(args) == 3{
         if args[2] == "--user"{
                 getInfoUser(args[1])
+                }else if args[2] == "--repo"{
+			getInfoRepo(args[1])
+			}
+                }else if len(args) == 2{
+                        if strings.Contains(args[1], "/"){
+                                getInfoRepo(args[1])
+                        }else{
+                                getInfoUser(args[1])
+                        }
                 }
-}
-	getInfoRepo("roistaff/go-mem-api")
+        }else{
+                welcome()
+
+        }
 }
